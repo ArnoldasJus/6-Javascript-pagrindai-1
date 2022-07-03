3216e6a61087b06e2a6529309131a497d19a9834
ArnoldasJus <50015456+ArnoldasJus@users.noreply.github.com>
2022-07-03 14:01:02 +0300

    Skaiciuotuvas su papildoma informacija

--- skaiciuotuvas.js
+++ skaiciuotuvas.js
@@ -1,21 +1,19 @@
 'use strict';
-// let skaicius1;
-// let skaicius2;
-// let suma;
-// let atimtis;
-// let sandauga;
-// let dalyba;
-// let skaiciuotuvo_rezultatas;
+
+// Lauku valymas
+
 function valyti(){
     document.getElementById("skaicius1").value = "";
     document.getElementById("skaicius2").value = "";
-    // document.getElementById("skaiciuotuvo_rezultatas").value = "";
+    document.getElementById("papildoma_informacija").value = "";
 };
 
 document.querySelector('#valyti').addEventListener('click', function(){
     console.log('Valymo mygtukas paspaustas');
     document.querySelector('#skaiciuotuvo_rezultatas').innerHTML = "";
 });
+
+// Aritmetiniai veiksmai
 
 document.querySelector('#sudeti').addEventListener('click', function(){
     console.log('Sudeties mygtukas paspaustas');
@@ -75,4 +73,24 @@ document.querySelector('#dalinti').addEventListener('click', function(){
     skaiciuotuvo_rezultatas = skaicius1 / skaicius2;
 
     document.querySelector('#skaiciuotuvo_rezultatas').append(skaiciuotuvo_rezultatas);
+});
+
+// Salygos sakiniai
+
+document.querySelector('#kuris_didziausias').addEventListener('click', function(){
+    console.log('Kuris didziausias mygtukas paspaustas');
+
+    let skaicius1;
+    let skaicius2;
+
+    skaicius1 = parseFloat(document.querySelector('#skaicius1').value);
+    skaicius2 = parseFloat(document.querySelector('#skaicius2').value);
+
+    if (skaicius1>skaicius2) {
+       document.querySelector('#papildoma_informacija').value = "Skaičius 1 yra didesnis";
+    } else if(skaicius2>skaicius1) {
+        document.querySelector('#papildoma_informacija').value = "Skaičius 2 yra didesnis";
+    } else {
+      document.querySelector('#papildoma_informacija').value = "Skaičiai yra lygūs";
+    }
 });
